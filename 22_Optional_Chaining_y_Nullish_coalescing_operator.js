d51bfc7d88f550d7036bc1f44b856ea790f87a7c
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-07-18 17:01:00 -0400

    Actualizando curso

---
+++ 22_Optional_Chaining_y_Nullish_coalescing_operator.js
@@ -0,0 +1,15 @@
+// Optional chaining ' ? '
+const alumno = {
+    nombre: 'Carlos',
+    clase: 'Programación 1',
+    aprobado: true
+}
+
+console.log
+
+
+
+
+
+
+// Nullish coalescing operator  ( ?? )

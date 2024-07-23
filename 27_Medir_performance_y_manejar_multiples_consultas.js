579968f7993e7f134635cca431597049cae3eddb
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-07-23 17:25:57 -0400

    Actualizando Curso

--- 27_Medir_performance_y_manejar_multiples_consultas.js
+++ 27_Medir_performance_y_manejar_multiples_consultas.js
@@ -25,29 +25,29 @@ const url3 = "https://jsonplaceholder.typicode.com/photos";
 // consultarAPI()
 
 //Error al poner tres consultas porque se tarda mas en responder
-const consultarAPI = async () => {
-  try {
-    const inicio = performance.now();
+// const consultarAPI = async () => {
+//   try {
+//     const inicio = performance.now();
 
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data);
 
-    const response2 = await fetch(url2);
-    const data2 = await response2.json();
-    console.log(data2);
+//     const response2 = await fetch(url2);
+//     const data2 = await response2.json();
+//     console.log(data2);
 
-    const response3 = await fetch(url3);
-    const data3 = await response3.json();
-    console.log(data3);
+//     const response3 = await fetch(url3);
+//     const data3 = await response3.json();
+//     console.log(data3);
 
-    const fin = performance.now();
-    console.log(`El resultado de la primera funcion es: ${fin - inicio} ms`);
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-consultarAPI();
+//     const fin = performance.now();
+//     console.log(`El resultado de la primera funcion es: ${fin - inicio} ms`);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+// consultarAPI();
 
 //Una consulta mas rapida
 const consultarAPI2 = async () => {
@@ -59,7 +59,17 @@ const consultarAPI2 = async () => {
       fetch(url2),
       fetch(url3),
     ]);
-
+    //Esta parte es opcional si quieres saber en que link puede haber un error 
+    if (!response.ok) {
+        throw new Error('Error al obtener datos de la URL: ' + url);
+      }
+      if (!response2.ok) {
+        throw new Error('Error al obtener datos de la URL: ' + url2);
+      }
+      if (!response3.ok) {
+        throw new Error('Error al obtener datos de la URL: ' + url3);
+      }
+    //Esta parte es opcional si quieres saber en que link puede haber un error 
     const [data, data2, data3] = await Promise.all([
       response.json(),
       response2.json(),
@@ -77,4 +87,4 @@ const consultarAPI2 = async () => {
   }
 };
 
-consultarAPI2();
+consultarAPI2()
---
+++ 28_DOM_Seleccionar_Elementos.js
@@ -0,0 +1,6 @@
+//Selectores
+//Todas las interraciones que haces en el DOM o en el codigo HTML por medio de JavaScript requieren o inician con un selector
+//Antes usabamos document.getElementById que te permite seleccionar un elemento HTML que tenga un ID unico 
+//Lo recomendado hoy en dia es document.querySelector() ... porque tienes acceso a selectores muy similares a los de CSS
+const heading = document.querySelector('.heading')
+console.log(heading)
